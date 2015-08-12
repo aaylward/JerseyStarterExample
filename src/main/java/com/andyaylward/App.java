c05e7469f400ab3c5b38f1fc9d3a4cdb1534ffc1
Andy Aylward <aaylward@gmail.com>
2015-08-12 02:03:06 -0400

    main doesn't throw anymore

--- src/main/java/com/andyaylward/App.java
+++ src/main/java/com/andyaylward/App.java
@@ -4,7 +4,7 @@ import ninja.freethrow.jerseystarter.JerseyStarter;
 import ninja.freethrow.jerseystarter.config.StartupConfiguration;
 
 public class App extends JerseyStarter {
-  public static void main(String... args) throws Exception {
+  public static void main(String... args) {
     StartupConfiguration configuration = StartupConfiguration
         .fromEnvWithBasePackage(App.class.getPackage());
     new App().configure(configuration).run();
